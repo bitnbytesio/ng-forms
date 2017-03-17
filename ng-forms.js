18325b5de4441b50477e8ebe1fdbec087483ad91
Harcharan Singh <artisangang@gmail.com>
2017-03-17 19:01:00 +0530

    v1.5
    validation error handling for lumen added

--- ng-forms.js
+++ ng-forms.js
@@ -1,7 +1,7 @@
 /*
  * ng-forms
  * @author: Harcharan Singh <artisangang@gmail.com>
- * @version 1.4
+ * @version 1.5
  * @git: https://github.com/artisangang/ng-forms
  */
 
@@ -10,6 +10,10 @@
     angular.module('ngForms', ['ng']).provider('$ngForm', function () {
 
 
+        this.callbacks = {
+                        success: function () {}
+
+                    };
       
 
         this.$get = ['$http','$location','$httpParamSerializerJQLike', function ($http, $location, $httpParamSerializerJQLike) {
@@ -18,6 +22,8 @@
 
             var $http = $http;
             var $location = $location;
+
+            var callbacks = this.callbacks;
 
             ngForm.create = function ($config, $callbacks) {
 
@@ -31,9 +37,7 @@
 
                     this.internalScope = {};
 
-                    this.callbacks = angular.extend({
-                        success: function () {}
-                    }, $callbacks);
+                    this.callbacks = angular.extend(callbacks, $callbacks);
 
                     this.config = angular.extend({
                         url: $config.url || '/',
@@ -45,9 +49,9 @@
 
                     if (typeof this.config.scope != 'undefined' && typeof this.config.scope[this.config.path] != 'undefined') {
 
-                    	this.data = this.config.scope[this.config.path];
+                        this.data = this.config.scope[this.config.path];
 
-                    	this.internalScope = this.config.scope;
+                        this.internalScope = this.config.scope;
 
                         var ref_path = this.config.path;
 
@@ -62,7 +66,7 @@
 
                 ngFormHandler.prototype.handle = function () {
 
-                	var ngFormInstance = this;
+                    var ngFormInstance = this;
                     var config = ngFormInstance.config;
                     var callbacks = ngFormInstance.callbacks;
 
@@ -90,14 +94,14 @@
                    } else {
                         config.headers['Content-Type'] = 'application/x-www-form-urlencoded;';
                         config.params = ngFormInstance.data;
-						config.paramSerializer = '$httpParamSerializerJQLike';
+                        config.paramSerializer = '$httpParamSerializerJQLike';
                     }
 
 
                     $http(config).then(function (o) {
 
-                	var o = o.data;
-			    
+                    var o = o.data;
+                
                         ngFormInstance.internalScope[config.path].response = o;
 
                         callbacks.success(o);
@@ -120,7 +124,7 @@
                         if (o.errors) {
 
                             if (typeof callbacks.errorHandler == 'function') {
-                                callbacks.errorHandler(o.errors);
+                                callbacks.errorHandler(o.errors, ngFormInstance.path, ngFormInstance.scope);
                             } 
 
                         }
@@ -128,15 +132,46 @@
                         
 
                         if (typeof o.alert != undefined) {
-                        	    ngFormInstance.internalScope.dismiss = function () {
-                        		ngFormInstance.internalScope[config.path].response.alert = null;
-                        	}
+                                ngFormInstance.internalScope.dismiss = function () {
+                                ngFormInstance.internalScope[config.path].response.alert = null;
+                            }
                         }
                         
 
 
                     }, function (err) {
-                        console.log(err);
+
+                        var o = {};
+
+                        if (err.status == 422) {
+
+                            ngFormInstance.internalScope[config.path].response = {};
+
+                            ngFormInstance.internalScope[config.path].response.errors = err.data;
+
+                            ngFormInstance.internalScope[config.path].response.hasError = function (key) {
+                                return typeof err.data[key] != 'undefined';
+                            };
+
+                            ngFormInstance.internalScope[config.path].response.error = function (key) {
+
+                       
+                                if (typeof err.data[key] == 'undefined') return;
+
+                                return (err.data[key] == 'String') ? err.data[key] : err.data[key][0];
+                            };
+
+                            if (typeof callbacks.errorHandler == 'function') {
+                                callbacks.errorHandler(err.data, ngFormInstance.path, ngFormInstance.scope);
+                            } 
+
+
+                        }
+                        
+                        if (typeof callbacks.exceptionHandler == 'function') {
+                                callbacks.exceptionHandler(err);
+                        } 
+
                     });
                 };
 
