a0e20d69ae63eda59dcb4eb9b62bec4e9c45e1b7
Harcharan Singh <artisangang@gmail.com>
2017-03-21 18:05:43 +0530

    temp changes

--- ng-forms.js
+++ ng-forms.js
@@ -183,47 +183,37 @@
         }];
 
     }).directive("fileModel", ['$parse', function ($parse) {
-        return {
-            restrict: 'A',
-            scope: {
-                fileModel: "="
-            },
-            link: function (scope, element, attrs) {
-                var model = $parse(attrs.fileModel);
-                var isMultiple = attrs.multiple;
-                var modelSetter = model.assign;
-                
-                element.bind("change", function (changeEvent) {
-                    
-                    var values = [];
-                    
-                    angular.forEach(element[0].files, function (item) {
-                        var value = {
-                           // File Name 
-                            name: item.name,
-                            //File Size 
-                            size: item.size,
-                            //File URL to view 
-                            url: URL.createObjectURL(item),
-                            // File Input Value 
-                            _file: item
-                        };
-                        values.push(value);
-                    });
-                    
-                    scope.$apply(function () {
-                         if (isMultiple) {
-                             scope.fileModel = element[0].files;
-                         } else {
-                             scope.fileModel = element[0].files[0];
-                         }
-
-                    });
-
-
+            return {
+        restrict: 'A',
+        link: function (scope, element, attrs) {
+            var model = $parse(attrs.ngFileModel);
+            var isMultiple = attrs.multiple;
+            var modelSetter = model.assign;
+            element.bind('change', function () {
+                var values = [];
+                angular.forEach(element[0].files, function (item) {
+                    var value = {
+                       // File Name 
+                        name: item.name,
+                        //File Size 
+                        size: item.size,
+                        //File URL to view 
+                        url: URL.createObjectURL(item),
+                        // File Input Value 
+                        _file: item
+                    };
+                    values.push(value);
                 });
-            }
+                scope.$apply(function () {
+                    if (isMultiple) {
+                        modelSetter(scope, values);
+                    } else {
+                        modelSetter(scope, values[0]);
+                    }
+                });
+            });
         }
+    };
 
     }]);
 
